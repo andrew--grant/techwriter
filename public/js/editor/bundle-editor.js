a6390d745583254edae76cda9db8777345fe1c5f
Andrew Grant <agrant@gme.net.au>
2015-10-09 16:52:05 +1100

    friday check in - css edits only

--- public/js/editor/bundle-editor.js
+++ public/js/editor/bundle-editor.js
@@ -13160,121 +13160,6 @@ module.exports = ContrastControl;
 var Quill = require('quill');
 var superagent = require('superagent');
 var ContrastControl = require('./contrast-control.js');
-var NotificationSystem = require('react-notification-system');
-
-// todo: editor alternatives
-// http://alloyeditor.com/
-// http://summernote.org/
-
-// todo: further research:
-// http://www.advancedfictionwriting.com/articles/snowflake-method/
-// http://www.advancedfictionwriting.com/product/snowflake-pro-software/
-
-// todo: autosaving
-// https://github.com/share/ShareJS
-// http://operational-transformation.github.io/ot-for-javascript.html
-
-var Editor = React.createClass({
-	displayName: 'Editor',
-
-	getInitialState: function getInitialState() {
-		return {
-			contents: null,
-			chapterid: null,
-			isLoadingContents: true,
-			isInitialPageLoad: true,
-			editor: null
-		};
-	},
-
-	componentDidMount: function componentDidMount() {
-		var editor = new Quill('#editor-container', {
-			modules: {
-				'toolbar': { container: '#formatting-container' }
-			},
-			theme: 'snow',
-			poll: 200
-		});
-		this.state.editor = editor;
-		this.loadContents();
-		this._notificationSystem = this.refs.notificationSystem;
-	},
-
-	loadContents: function loadContents(evt) {
-		var self = this;
-		var chapterid = location.search.split('=')[1];
-		if (chapterid) {
-			this.state.chapterid = chapterid;
-			superagent.get('/editorload/?chapterid=' + chapterid).end(function (err, res) {
-				if (err) {
-					console.log("error");
-					console.log(err);
-				} else {
-					var status = res.body.status;
-					if (status == "success") {
-						console.log(res.body.content);
-						self.state.editor.setContents(JSON.parse(res.body.content));
-					} else {
-						console.log("status: " + status);
-					}
-				}
-			});
-		} else {
-			// new document
-			self.state.editor.focus();
-		}
-	},
-
-	saveContents: function saveContents(evt) {
-		// todo: local saves offline??
-		var self = this;
-		var contents = this.state.editor.getContents();
-		console.log(JSON.stringify(this.state.editor.getContents()));
-		superagent.post('/editorsave/?chapterid=' + this.state.chapterid).send({ contents: contents }).end(function (err, res) {
-			if (err) {
-				console.log("error");
-				console.log(err);
-				self.unsuccesfullSaveNotification();
-			} else {
-				console.log("success");
-				self.succesfullSaveNotification();
-			}
-		});
-	},
-
-	closeEditor: function closeEditor() {
-		// todo: snapshot save / warnings etc
-		location.href = "/";
-	},
-
-	succesfullSaveNotification: function succesfullSaveNotification() {
-		this._notificationSystem.addNotification({
-			message: 'Your Work has been Saved',
-			level: 'info',
-			position: 'br'
-		});
-	},
-
-	unsuccesfullSaveNotification: function unsuccesfullSaveNotification() {
-		this._notificationSystem.addNotification({
-			message: 'Your Work could not be Saved',
-			level: 'error',
-			position: 'br',
-			autoDismiss: 0
-		});
-	},
-
-	render: function render() {
-		return React.createElement(
-			'div',
-			{ id: 'editor-wrapper' },
-			React.createElement(EditorToolbar, { onSave: this.saveContents,
-				onClose: this.closeEditor }),
-			React.createElement('div', { id: 'editor-container' }),
-			React.createElement(NotificationSystem, { ref: 'notificationSystem' })
-		);
-	}
-});
 
 var EditorToolbar = React.createClass({
 	displayName: 'EditorToolbar',
@@ -13399,11 +13284,143 @@ var EditorToolbar = React.createClass({
 	}
 });
 
+module.exports = EditorToolbar;
+
+},{"./contrast-control.js":14,"quill":2,"superagent":10}],16:[function(require,module,exports){
+"use strict";
+var Quill = require('quill');
+var EditorToolbar = require('./editor-toolbar.js');
+var superagent = require('superagent');
+var NotificationSystem = require('react-notification-system');
+
+// todo: editor alternatives
+// http://alloyeditor.com/
+// http://summernote.org/
+
+// todo: further research:
+// http://www.advancedfictionwriting.com/articles/snowflake-method/
+// http://www.advancedfictionwriting.com/product/snowflake-pro-software/
+
+// todo: autosaving
+// https://github.com/share/ShareJS
+// http://operational-transformation.github.io/ot-for-javascript.html
+
+var Editor = React.createClass({
+	displayName: 'Editor',
+
+	getInitialState: function getInitialState() {
+		return {
+			contents: null,
+			chapterid: null,
+			isLoadingContents: true,
+			isInitialPageLoad: true,
+			editor: null
+		};
+	},
+
+	componentDidMount: function componentDidMount() {
+		var editor = new Quill('#quill', {
+			modules: {
+				'toolbar': { container: '#formatting-container' }
+			},
+			theme: 'snow',
+			poll: 200
+		});
+		this.state.editor = editor;
+		this.loadContents();
+		this._notificationSystem = this.refs.notificationSystem;
+	},
+
+	loadContents: function loadContents(evt) {
+		var self = this;
+		var chapterid = location.search.split('=')[1];
+		if (chapterid) {
+			this.state.chapterid = chapterid;
+			superagent.get('/editorload/?chapterid=' + chapterid).end(function (err, res) {
+				if (err) {
+					console.log("error");
+					console.log(err);
+				} else {
+					var status = res.body.status;
+					if (status == "success") {
+						console.log(res.body.content);
+						self.state.editor.setContents(JSON.parse(res.body.content));
+					} else {
+						console.log("status: " + status);
+					}
+				}
+			});
+		} else {
+			// new document
+			self.state.editor.focus();
+		}
+	},
+
+	saveContents: function saveContents(evt) {
+		// todo: local saves offline??
+		var self = this;
+		var contents = this.state.editor.getContents();
+		console.log(JSON.stringify(this.state.editor.getContents()));
+		superagent.post('/editorsave/?chapterid=' + this.state.chapterid).send({ contents: contents }).end(function (err, res) {
+			if (err) {
+				console.log("error");
+				console.log(err);
+				self.unsuccesfullSaveNotification();
+			} else {
+				console.log("success");
+				self.succesfullSaveNotification();
+			}
+		});
+	},
+
+	closeEditor: function closeEditor() {
+		// todo: snapshot save / warnings etc
+		location.href = "/";
+	},
+
+	succesfullSaveNotification: function succesfullSaveNotification() {
+		this._notificationSystem.addNotification({
+			message: 'Your Work has been Saved',
+			level: 'info',
+			position: 'br'
+		});
+	},
+
+	unsuccesfullSaveNotification: function unsuccesfullSaveNotification() {
+		this._notificationSystem.addNotification({
+			message: 'Your Work could not be Saved',
+			level: 'error',
+			position: 'br',
+			autoDismiss: 0
+		});
+	},
+
+	render: function render() {
+
+		return React.createElement(
+			'div',
+			{ id: 'editor-wrapper' },
+			React.createElement(
+				'div',
+				{ id: 'editor-toolbar' },
+				React.createElement(EditorToolbar, { onSave: this.saveContents, onClose: this.closeEditor })
+			),
+			React.createElement(
+				'div',
+				{ id: 'editor' },
+				React.createElement('div', { id: 'quill' })
+			),
+			React.createElement(NotificationSystem, { ref: 'notificationSystem' })
+		);
+	}
+
+});
+
 React.render(React.createElement(Editor, null), document.getElementById('editorMount'));
 
-},{"./contrast-control.js":14,"quill":2,"react-notification-system":7,"superagent":10}],16:[function(require,module,exports){
+},{"./editor-toolbar.js":15,"quill":2,"react-notification-system":7,"superagent":10}],17:[function(require,module,exports){
 "use strict";
 require('./editor.js');
 require('./assets.js');
 
-},{"./assets.js":13,"./editor.js":15}]},{},[16]);
+},{"./assets.js":13,"./editor.js":16}]},{},[17]);
